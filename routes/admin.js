4be49246989b3ba1378f72e4511fbeec11f24346
Felipe <feventurelli@hotmail.com>
2020-09-18 16:41:25 -0300

    configuracao dashboard e exibicao dos dados do usuario adm

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var users = require('./../inc/users');
 var admin = require('./../inc/admin');
+var menus = require('./../inc/menus');
 var router = express.Router();
 
 router.use(function(req, res, next){
@@ -32,8 +33,14 @@ router.get("/logout", function(req, res, next){
 
 router.get('/', function(req, res, next){
 
-    res.render("admin/index", {
-        menus: req.menus
+    admin.dashboard().then(data=>{
+
+        res.render("admin/index", admin.getParams(req, {
+            data
+        }));
+
+    }).catch(err=>{
+        console.error(err);
     });
 
 });
@@ -67,42 +74,45 @@ router.get('/login', function(req, res, next){
 
 router.get('/contacts', function(req, res, next){
 
-    res.render("admin/contacts", {
-        menus: req.menus
-    });
+    res.render("admin/contacts", admin.getParams(req));
 
 });
 
 router.get('/emails', function(req, res, next){
 
-    res.render("admin/emails", {
-        menus: req.menus
-    });
+    res.render("admin/emails", admin.getParams(req));
 
 });
 
 router.get('/menus', function(req, res, next){
 
-    res.render("admin/menus", {
-        menus: req.menus
+    menus.getMenus().then(data => {
+
+        res.render("admin/menus", admin.getParams(req, {
+            data
+        }));
+
     });
+
+});
+
+router.post('/menus', function(req, res, next){
+
+    res.send(req.body);
 
 });
 
 router.get('/reservations', function(req, res, next){
 
-    res.render("admin/reservations", {
-        date: {},
-        menus: req.menus
-    });
+    res.render("admin/reservations", admin.getParams(req, {
+        date: {}
+    }));
 
 });
 
 router.get('/users', function(req, res, next){
 
-    res.render("admin/users", {
-        menus: req.menus
-    });
+    res.render("admin/users", admin.getParams(req));
 
 });
 
