ece961567ea0ca252b996061ae69886ca758c1a4
Felipe <feventurelli@hotmail.com>
2020-09-29 13:22:07 -0300

    Update e Delete usuários e delete contatos - administração

--- public/js/hcode-grid.js
+++ public/js/hcode-grid.js
@@ -2,12 +2,39 @@ class HcodeGrid{
 
     constructor(configs){
 
+        configs.listeners = Object.assign({
+            afterUpdateClick: (e)=>{
+                $('#modal-update').modal('show');
+            },
+            afterDeleteClick: (e)=>{
+                window.location.reload();
+            },
+            afterFormCreate: (e)=>{
+                window.location.reload();
+            },
+            afterFormUpdate: (e)=>{
+                window.location.reload();
+            },
+            afterFormCreateError: (e)=>{
+                alert("Não foi possível enviar o formulário");
+            },
+            afterFormUpdateError: (e)=>{
+                alert("Não foi possível enviar o formulário");
+            }
+        }, configs.listeners);
+
         this.options = Object.assign({}, {
             formCreate:"#modal-create form",
             formUpdate:"#modal-update form",
-            btnUpdate:'.btn-update',
-            btnDelete:'.btn-delete',
+            btnUpdate:'btn-update',
+            btnDelete:'btn-delete',
+            onUpdateLoad: (form, name, data)=> {
+                let input = form.querySelector('[name=' + name + ']');
+                if(input) input.value = data[name];
+            }
         }, configs);
+
+        this.rows = [...document.querySelectorAll('table tbody tr')];
 
         this.initForms();
         this.initButtons();
@@ -18,91 +45,121 @@ class HcodeGrid{
 
         this.formCreate = document.querySelector(this.options.formCreate);
 
-        this.formCreate.save().then(json=>{
+        if(this.formCreate){
 
-            window.location.reload();
+            this.formCreate.save({
+                success: ()=>{
+                    this.fireEvents('afterFormCreate');
+                },
+                failure: ()=>{
+                    this.fireEvents('afterFormCreateError');
+                }
+            });
 
-        }).catch(err=>{
-            console.log(err);
-        });
-
+        }
 
         this.formUpdate = document.querySelector(this.options.formUpdate);
 
-        this.formUpdate.save().then(json=>{
+        if(this.formUpdate){
 
-            window.location.reload();
+            this.formUpdate.save({
+                success: ()=>{
+                    this.fireEvents('afterFormUpdate');
+                },
+                failure: ()=>{
+                    this.fireEvents('afterFormUpdateError');
+                }
+            });
 
-        }).catch(err=>{
-            console.log(err);
+        }
+
+    }
+
+    fireEvents(name, args){
+        
+        if(typeof this.options.listeners[name] === 'function') this.options.listeners[name].apply(name, args);
+
+        
+    }
+
+    getTrData(e){
+
+        let tr = e.path.find(el=>{
+
+            return (el.tagName.toUpperCase() === 'TR');
+
         });
+
+        return JSON.parse(tr.dataset.row);
+
+    }
+
+    btnUpdateClick(e){
+
+        this.fireEvents('beforeUpdateClick', [e]);
+
+        let data = this.getTrData(e);
+
+        for(let name in data){
+
+            this.options.onUpdateLoad(this.formUpdate, name, data);
+            
+        }
+
+        this.fireEvents('afterUpdateClick', [e]);
+
+    }
+
+    btnDeleteClick(e){
+
+        this.fireEvents('beforeDeleteClick');
+
+        let data = this.getTrData(e);
+
+        if(confirm(eval('`' + this.options.deleteMsg + '`'))){
+
+            fetch(eval('`' + this.options.deleteUrl + '`'), {
+            method: "DELETE"
+            })
+            .then(response => response.json())
+            .then(json=>{
+
+                this.fireEvents('afterDeleteClick');
+
+            });
+
+        }
 
     }
 
     initButtons(){
 
-        [...document.querySelectorAll(this.options.btnUpdate)].forEach(btn=>{
+        this.rows.forEach(row=>{
 
-            btn.addEventListener('click', e =>{
+            [...row.querySelectorAll('.btn')].forEach(btn=>{
 
-            let tr = e.path.find(el=>{
+                btn.addEventListener('click', e => {
 
-                return (el.tagName.toUpperCase() === 'TR');
+                    if(e.target.classList.contains(this.options.btnUpdate)){
 
-            });
+                        this.btnUpdateClick(e)
 
-            let data = JSON.parse(tr.dataset.row);
+                    }else if(e.target.classList.contains(this.options.btnDelete)){
 
-            for(let name in data){
+                        this.btnDeleteClick(e);
 
-            let input = this.formUpdate.querySelector(`[name=${name}]`);
+                    }else{
+                        
+                        this.fireEvents('buttonClick', [e.target, this.getTrData(e), e]);
+                        
+                    }
 
-            switch(name){
-
-                case 'date':
-                    if (input) input.value = moment(data[name]).format("YYYY-MM-DD");
-                break;
-
-                default:
-                
-                if (input) input.value = data[name];
-            }
-
-            }
-
-            $('#modal-update').modal('show');
-
-            });
-
-        });
-
-        [...document.querySelectorAll(this.options.btnDelete)].forEach(btn=>{
-
-            btn.addEventListener('click', e=>{
-
-            let tr = e.path.find(el=>{
-
-                return (el.tagName.toUpperCase() === 'TR');
-
-            });
-
-            let data = JSON.parse(tr.dataset.row);
-
-            if(confirm(eval('`' + this.options.deleteMsg + '`'))){
-
-                fetch(eval('`' + this.options.deleteUrl + '`'), {
-                method: "DELETE"
-                })
-                .then(response => response.json())
-                .then(json=>{
-                window.location.reload();
                 });
 
-            }
-
             });
 
         });
+
 
     }
 
--- routes/admin.js
+++ routes/admin.js
@@ -3,6 +3,7 @@ var users = require('./../inc/users');
 var admin = require('./../inc/admin');
 var menus = require('./../inc/menus');
 var reservations = require('./../inc/reservations');
+var contacts = require('./../inc/contacts');
 var moment = require('moment');
 var router = express.Router();
 
@@ -78,7 +79,24 @@ router.get('/login', function(req, res, next){
 
 router.get('/contacts', function(req, res, next){
 
-    res.render("admin/contacts", admin.getParams(req));
+    contacts.getContacts().then(data => {
+
+        res.render("admin/contacts", admin.getParams(req, {
+            data
+        }));
+
+    });
+
+
+});
+
+router.delete('/contacts/:id', function(req, res, next){
+
+    contacts.delete(req.params.id).then(results =>{
+        res.send(results);
+    }).catch(err=>{
+        res.send(err);
+    });
 
 });
 
@@ -162,9 +180,57 @@ router.delete("/reservations/:id", function(req, res, next){
 
 });
 
-router.get('/users', function(req, res, next){
+router.get('/users', (req, res, next) => {
 
-    res.render("admin/users", admin.getParams(req));
+    users.getUsers().then(data => {
+        res.render('admin/users', admin.getParams(req, {
+            data
+        }));
+    }); 
+
+});
+
+router.post('/users', function(req, res, next){
+
+    users.save(req.fields).then(results=>{
+
+        res.send(results);
+
+    }).catch(err=>{
+
+        res.send(err);
+
+    });
+
+});
+
+router.post('/users/password-change', function(req, res, next){
+
+    users.changePassword(req).then(results=>{
+
+        res.send(results);
+
+    }).catch(err=>{
+
+        res.send({
+            error: err
+        });
+
+    });
+
+});
+
+router.delete('/users/:id', function(req, res, next){
+
+    users.delete(req.params.id).then(results=>{
+
+        res.send(results);
+
+    }).catch(err=>{
+
+        res.send(err);
+
+    });
 
 });
 
